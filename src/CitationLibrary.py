381eb539c7f5ae03ed5591cfe321ae087d4155e2
kurkilmo <ilmo.kurki00@gmail.com>
2024-12-01 17:40:21 +0200

    Update CitationLibrary.py

--- src/CitationLibrary.py
+++ src/CitationLibrary.py
@@ -7,9 +7,9 @@ class CitationLibrary:
 
     def add_new_citation(self, author, title):
         """Add New Citation"""
-        citation = Citation(author=author, title=title)
+        citation = Citation("article", "key", {"author": author, "title": title})
         self.repo.add_new(citation)
 
     def get_all_citations(self):
         """Get All Citations"""
-        return [f"{c.author}: {c.title}" for c in self.repo.get_all()]
+        return [f"{c.fields["author"]}: {c.fields["title"]}" for c in self.repo.get_all()]
