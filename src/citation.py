412e00183bbaadcbeb28bb9885f6c10c99f0b0cf
kurkilmo <ilmo.kurki00@gmail.com>
2024-12-10 15:38:01 +0200

    Fix author print format fix

--- src/citation.py
+++ src/citation.py
@@ -14,8 +14,8 @@ class Citation:
     def __str__(self):
         result = f"@{self.citation_type}{{{self.key},\n"
         for key in self.fields.keys():
-            if key == "author":
-                result += f"    {key} = {{{" and ".join(self.fields[key])}}},\n"
+            if key == "authors":
+                result += f"    author = {{{" and ".join(self.fields[key])}}},\n"
             else:
                 result += f"    {key} = {{{self.fields[key]}}},\n"
         result = result[:-2]
